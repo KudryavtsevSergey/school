56218f3721cbc3273cf0e9b1c4f246168850bbb6
KudryavtsevSergey <invaderman@mail.ru>
2018-04-04 23:24:09 +0300

    add interfaces IGeneraor + IGeneratorService

---
+++ src/main/java/by/school/service/generation/IGenerator.java
@@ -0,0 +1,21 @@
+package by.school.service.document.generation;
+
+import by.school.entity.*;
+import by.school.service.exception.ServiceException;
+
+import java.io.OutputStream;
+import java.sql.Date;
+import java.util.List;
+
+public interface IGenerator {
+    OutputStream generateClassPupilListDocument(OutputStream os, Teacher teacher, Clazz clazz, List<Pupil> pupilList)
+            throws ServiceException;
+    OutputStream generateTeacherScheduleDocument(OutputStream os, Teacher teacher, List<SubjectInSchedule> subjectInScheduleList,
+                                                 List<LessonTime> lessonTimeList) throws ServiceException;
+    OutputStream generateClassScheduleDocument(OutputStream os, Clazz clazz, List<SubjectInSchedule> subjectInScheduleList,
+                                               List<LessonTime> lessonTimeList) throws ServiceException;
+    OutputStream generateFullScheduleDocument(OutputStream os, List<SubjectInSchedule> subjectInScheduleList,
+                                              List<LessonTime> lessonTimeList, List<Teacher> teacherList) throws ServiceException;
+    OutputStream generateMarksDocument(OutputStream os, Subject subject, List<Mark> markList, List<Pupil> pupilList, Clazz clazz,
+                                       List<Date> lessonDateList) throws ServiceException;
+}
